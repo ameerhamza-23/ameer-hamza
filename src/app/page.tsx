714aac6d3e7777c739e024f102b1b7795b584e58
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-05-10 01:14:07 +0500

    Added Jump Scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
 
-    <div>
+    <div className="scroll-parent">
       <section style={{ height: '100vh' }} className="flex justify-center items-center">
         <div className="size-3/4 flex items-center">
           <h1 className="text-2xl text-center">"Hello! I'm a passionate web developer committed to mastering new technologies to craft dynamic and responsive websites. Let's innovate together!"
