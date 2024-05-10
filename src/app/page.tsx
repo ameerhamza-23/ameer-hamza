4159df99d4b5a7faa0ffce53a840993b8d06e48d
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-05-10 15:15:53 +0500

    Added Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="scroll-parent">
       <section style={{ height: '100vh' }} className="flex justify-center items-center">
         <div className="size-3/4 flex items-center">
-          <h1 className="text-2xl text-center">"Hello! I'm a passionate web developer committed to mastering new technologies to craft dynamic and responsive websites. Let's innovate together!"
+          <h1 className="text-2xl text-center">Hello! I'm a passionate web developer committed to mastering new technologies to craft dynamic and responsive websites. Let's innovate together"
           </h1>
         </div>
       </section>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,53 @@
 export default function Skills() {
-    return <h1>Skills</h1>
+    return (
+        <div className="container">
+            <div className="hexagonArea first">
+                <div className="hexagon">
+                    <h3>S1</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S2</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S3</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S4</h3>
+                </div>
+            </div>
+
+            <div className="hexagonArea second">
+                <div className="hexagon">
+                    <h3>S5</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S6</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S7</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S3</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S4</h3>
+                </div>
+            </div>
+
+            <div className="hexagonArea first">
+                <div className="hexagon">
+                    <h3>S1</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S2</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S3</h3>
+                </div>
+                <div className="hexagon">
+                    <h3>S4</h3>
+                </div>
+            </div>
+        </div>
+    )
 }
